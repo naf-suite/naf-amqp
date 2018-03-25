58203c3b981e0fc384c9373648b7bf500898b6ff
dygapp <dygapp@qq.com>
2018-03-26 05:13:57 +0800

    0.0.10

--- index.d.ts
+++ index.d.ts
@@ -18,13 +18,32 @@ declare namespace Naf {
 
   export interface AmqpWrapper {
     /**
-     * publish to topic
+     * publish to exchage
+     * @param exchange exchange name
+     * @param type exchange type: fanout、topic
+     * @param routingKey key value
+     * @param content message content
+     * @param options options for exchange
+     */
+    exchage(exchange: string, type: string, routingKey: string, content: string, options: any = { durable: false });
+
+    /**
+     * publish to topic exchange
      * @param exchange exchange name
      * @param routingKey key value
      * @param content message content
      * @param options options for exchange
      */
     topic(exchange: string, routingKey: string, content: string, options: any = { durable: false });
+
+    /**
+     * publish to fanout exchage
+     * @param exchange exchange name
+     * @param routingKey key value
+     * @param content message content
+     * @param options options for exchange
+     */
+    fanout(exchange: string, routingKey: string, content: string, options: any = { durable: false });
 
     /**
      * publish to queue
